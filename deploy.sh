aedbf4daa068f44175a151c0aa468b5d47d34961
josiah <dev517584922@126.com>
2025-04-03 20:05:50 +0800

    feat: sh

--- deploy.sh
+++ deploy.sh
@@ -6,25 +6,27 @@ set -x
 # 设置变量
 DOCKER_IMAGE="austin-link"
 DOCKER_TAG="${BUILD_NUMBER}"
-DOCKER_CMD="/usr/bin/docker" # 使用宿主机的Docker命令
+
+# 确保有执行权限
+sudo chmod 666 /var/run/docker.sock
 
 echo "Starting deployment process..."
 
 # 构建Docker镜像
 echo "Building Docker image..."
-${DOCKER_CMD} build -t ${DOCKER_IMAGE}:${DOCKER_TAG} . || {
+docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} . || {
     echo "Docker build failed"
     exit 1
 }
 
 # 停止并删除旧容器（如果存在）
 echo "Stopping and removing old container..."
-${DOCKER_CMD} stop ${DOCKER_IMAGE} || true
-${DOCKER_CMD} rm ${DOCKER_IMAGE} || true
+docker stop ${DOCKER_IMAGE} || true
+docker rm ${DOCKER_IMAGE} || true
 
 # 运行新容器
 echo "Starting new container..."
-${DOCKER_CMD} run -d \
+docker run -d \
     --name ${DOCKER_IMAGE} \
     -p 8090:8090 \
     --restart unless-stopped \
